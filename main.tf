11a3999750efcc3dd3990951f7681cf5bbbab353
phozzy <arthur.fayzullin@gmail.com>
2020-04-23 20:55:15 +0200

    Remove Interpolation-only expressions

--- main.tf
+++ main.tf
@@ -12,25 +12,25 @@ resource "aws_vpc" "EC2VPCPIONEER" {
   cidr_block = "10.0.0.0/16"
 }
 resource "aws_subnet" "EC2SPIONEER01" {
-  vpc_id = "${aws_vpc.EC2VPCPIONEER.id}"
+  vpc_id = aws_vpc.EC2VPCPIONEER.id
   cidr_block = "10.0.1.0/24"
 }
 resource "aws_subnet" "EC2SPIONEER02" {
-  vpc_id = "${aws_vpc.EC2VPCPIONEER.id}"
+  vpc_id = aws_vpc.EC2VPCPIONEER.id
   cidr_block = "10.0.2.0/24"
 }
 resource "aws_directory_service_directory" "DSPIONEER" {
   name = "piohack.thehyve.net"
-  password = "${var.ds_admin_pass}"
+  password = var.ds_admin_pass
   size = "Small"
   vpc_settings {
-    vpc_id = "${aws_vpc.EC2VPCPIONEER.id}"
+    vpc_id = aws_vpc.EC2VPCPIONEER.id
     subnet_ids = [
-      "${aws_subnet.EC2SPIONEER01.id}",
-      "${aws_subnet.EC2SPIONEER02.id}"
+      aws_subnet.EC2SPIONEER01.id,
+      aws_subnet.EC2SPIONEER02.id
     ]
   }
 }
 resource "aws_workspaces_directory" "WSWPIONEER" {
-  directory_id = "${aws_directory_service_directory.DSPIONEER.id}"
+  directory_id = aws_directory_service_directory.DSPIONEER.id
 }
